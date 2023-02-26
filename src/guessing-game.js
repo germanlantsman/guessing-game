c194b0883adec8c229ea6407d937a1f853e4af8d
germanlantsman <germanlantsman@gmail.com>
2023-02-26 22:35:22 +0300

    feat: update solusion file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
     }
 
     guess() {
-
+        this.averageNumber = Math.round((this.minNumber + this.maxNumber)/2);
+        return this.averageNumber;
     }
 
     lower() {
-
+        this.maxNumber = this.averageNumber;
     }
 
     greater() {
-
+        this.minNumber = this.averageNumber;
     }
 }
 
